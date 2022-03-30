5e9dd3a82a98594e5475ad381fbe85621cc08224
mburucuya24 <92304084+mburucuya24@users.noreply.github.com>
2022-03-30 12:19:49 -0400

    Rendered the word examples and synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Example from "./Example";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
@@ -8,9 +10,11 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             <p>
+              <strong>Definition: </strong>
               {definition.definition}
               <br />
-              <em>{definition.example}</em>
+              <Example example={definition.example} />
+              <Synonyms synonyms={definition.synonyms} />
             </p>
           </div>
         );
